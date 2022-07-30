5c09211ccc3aa28d359035d560577d52f15ad79e
radopeti <rado.peti@gmail.com>
2022-07-30 19:09:52 +0200

    use KafkaService in ChatController

--- src/main/java/com/radopeti/actionmonitor/controller/ChatController.java
+++ src/main/java/com/radopeti/actionmonitor/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.radopeti.actionmonitor.controller;
 
 import com.radopeti.actionmonitor.model.ChatMessage;
+import com.radopeti.actionmonitor.service.KafkaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -12,10 +13,17 @@ public class ChatController {
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    private final KafkaService kafkaService;
+
+    public ChatController(KafkaService kafkaService) {
+        this.kafkaService = kafkaService;
+    }
+
     @MessageMapping("/chat")
     @SendTo("/topic/messages")
     public String receiveChatMessage(ChatMessage message) {
         logger.info(message.getContent());
+        kafkaService.sendMsg(message.getContent());
         return message.getContent();
     }
 }
