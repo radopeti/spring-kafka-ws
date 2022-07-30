4074f691eb98572ceecc44745b446b6db93cb263
radopeti <rado.peti@gmail.com>
2022-07-30 12:43:59 +0200

    init project

---
+++ src/main/java/com/radopeti/actionmonitor/ActionMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.radopeti.actionmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/radopeti/actionmonitor/ActionMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radopeti.actionmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
