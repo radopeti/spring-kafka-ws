3ed032d5a6a87f911533bc1ad35f955177024bdd
radopeti <rado.peti@gmail.com>
2022-07-31 17:08:55 +0200

    configure health and info actuator endpoints

---
+++ src/main/java/com/radopeti/actionmonitor/component/KafkaHealthIndicator.java
@@ -0,0 +1,51 @@
+package com.radopeti.actionmonitor.component;
+
+import com.radopeti.actionmonitor.model.ChatAction;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.DescribeClusterOptions;
+import org.apache.kafka.clients.admin.DescribeClusterResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutionException;
+
+@Component
+public class KafkaHealthIndicator implements HealthIndicator {
+
+    private final KafkaAdmin admin;
+
+    private final AdminClient adminClient;
+
+    public KafkaHealthIndicator(KafkaAdmin admin) {
+        this.admin = admin;
+        this.adminClient = AdminClient.create(admin.getConfigurationProperties());
+    }
+
+    /**
+     * Return an indication of health.
+     *
+     * @return the health for
+     */
+    @Override
+    public Health health() {
+        final DescribeClusterOptions describeClusterOptions = new DescribeClusterOptions().timeoutMs(1000);
+        final DescribeClusterResult describeCluster = adminClient.describeCluster(describeClusterOptions);
+        try {
+            final String clusterId = describeCluster.clusterId().get();
+            final int nodeCount = describeCluster.nodes().get().size();
+            return Health.up()
+                    .withDetail("clusterId", clusterId)
+                    .withDetail("nodeCount", nodeCount)
+                    .build();
+        } catch (InterruptedException | ExecutionException e) {
+            return Health.down()
+                    .withException(e)
+                    .build();
+        }
+    }
+}
